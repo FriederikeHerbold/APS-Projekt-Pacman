b32e85b6dffea880627bec1df6d0a17e4be21e47
julianbusch <insane.bluefin@gmx.de>
2017-03-27 06:39:00 +0200

    images eingebunden
    images eingebunden

---
+++ PacTheKnowledgeJulian3/src/PacTheKnowledge/SuperTimer.java
@@ -0,0 +1,20 @@
+package PacTheKnowledge;
+
+public class SuperTimer extends Thread {
+
+	Student student;
+	
+	public SuperTimer(Student student){
+		this.student=student;
+	}
+	
+	public void run(){
+		try {
+			sleep(10000);
+			student.normalize();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
